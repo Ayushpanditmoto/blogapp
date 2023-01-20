f4885456f5c4c741179064eac6ae589d4929f49c
Ayush Kumar Pandit <ayushpanditmoto@gmail.com>
2023-01-20 21:12:35 +0530

    feat fixed

--- src/components/Addblog.js
+++ src/components/Addblog.js
@@ -1,80 +1,100 @@
-import React, { useState } from 'react'
-import {addDoc,collection,Timestamp} from "firebase/firestore"
-import {db} from "../firebaseConfig"
-
+import React, { useState } from "react";
+import { addDoc, collection, Timestamp } from "firebase/firestore";
+import { db } from "../firebaseConfig";
 
 const Addblog = () => {
+  const [createdby, setCreatedby] = useState("");
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [imgurl, setImgurl] = useState("");
+  const [createdat, setCreatedat] = useState(
+    Timestamp.now().toDate().toString()
+  );
 
-    const [createdby,setCreatedby]=useState("");
-    const [title,setTitle]=useState("");
-    const [description,setDescription]=useState("");
-    const [imgurl,setImgurl]=useState("");
-    const [createdat,setCreatedat]=useState(Timestamp.now().toDate().toString());
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-
-    const handleSubmit=async (e)=>{
-        e.preventDefault();
-
-        if(title=="" || description=="" || createdby=="")
-        {
-            alert("Must fill everything");
-            return;
-        }
-
-
-        await addDoc(collection(db,"Blogs"),{
-           
-            title,createdat,createdby,description,imgurl
-
-        }).then(()=>{alert("Success!")})
-        .catch(err=>{alert(err.message)})
-
-        //setTitle("");
-        setCreatedat("");
-        setCreatedby("");
-        setTitle("");
-        setImgurl("");
-
-        const form=document.getElementById('my_form');
-        form.reset();
-     
-
+    if (title === "" || description === "" || createdby === "") {
+      alert("Must fill everything");
+      return;
     }
 
-    return (
-        <div className='addblog-form'>
-            <p>Add a new Blog</p>
-            <form onSubmit={handleSubmit} id="my_form">
+    await addDoc(collection(db, "Blogs"), {
+      title,
+      createdat,
+      createdby,
+      description,
+      imgurl,
+    })
+      .then(() => {
+        alert("Success!");
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
 
-                 <label>Name</label>
-                 <input name="username" placeholder="Enter your name" type="text" onChange={(e)=>{setCreatedby(e.target.value)}} /> 
-                 <br />
+    //setTitle("");
+    setCreatedat("");
+    setCreatedby("");
+    setTitle("");
+    setImgurl("");
 
-                 <label>Blog Title</label>
-                 <input name="title" placeholder="Enter Blog title" type="text" onChange={(e)=>{setTitle(e.target.value)}} /> 
-                 <br />
+    const form = document.getElementById("my_form");
+    form.reset();
+  };
 
-                 <label>Blog Description</label>
-                 <textarea onChange={(e)=>{setDescription(e.target.value)}} type="text" placeholder='Write your article'></textarea>
-                 <br />
+  return (
+    <div className="addblog-form">
+      <p>Add a new Blog</p>
+      <form onSubmit={handleSubmit} id="my_form">
+        <label>Name</label>
+        <input
+          name="username"
+          placeholder="Enter your name"
+          type="text"
+          onChange={(e) => {
+            setCreatedby(e.target.value);
+          }}
+        />
+        <br />
 
-                 <label>Paste Image link</label>
-                 <input name="image" placeholder="Paste image url" type="text" onChange={(e)=>{setImgurl(e.target.value)}} /> 
-                 
-                 <div className='btn-cotainer'>
-         
-                    <button>Submit</button>
+        <label>Blog Title</label>
+        <input
+          name="title"
+          placeholder="Enter Blog title"
+          type="text"
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+        <br />
 
-                 </div>
+        <label>Blog Description</label>
+        <textarea
+          onChange={(e) => {
+            setDescription(e.target.value);
+          }}
+          type="text"
+          placeholder="Write your article"
+        ></textarea>
+        <br />
 
+        <label>Paste Image link</label>
+        <input
+          name="image"
+          placeholder="Paste image url"
+          type="text"
+          onChange={(e) => {
+            setImgurl(e.target.value);
+          }}
+        />
 
-                 
-
-            </form>
-
-
+        <div className="btn-cotainer">
+          <button>Submit</button>
         </div>
-    )
-}
+      </form>
+    </div>
+  );
+};
 
-export default Addblog
+export default Addblog;
